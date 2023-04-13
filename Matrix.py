07441b69fe635f3a725ccd268832ead7585e51a4
saisrinivas3g <sai.srinivas3g@live.in>
2023-04-13 21:15:25 +0530

    Update Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -6,22 +6,47 @@ def matrix(a,b):
         print("Enter how many columns are there in "+chr(65+i)+" :")
         x2 = int(input())
         a1 += [[x1,x2]]
-    b2 = b == "+" or b == "-"
+    b2 = (b == "+") or (b == "-")
     if b2:
-        if a1[0]==a1[1]:
-            p = " "+b+" operation of the matricess is possible"
+        if (a1[0]==a1[1]):
+            # p = " "+b+" operation of the matricess is possible"
             # print(a1)
+            p = add_sub(a1,b)
 
         else:
             p = " "+b+" operation of the matricess is not possible"
         
     else:
-        if a1[0][1]==a1[1][0]:
+        if (a1[0][1]==a1[1][0]):
             p = " "+b+" operation of the matricess is possible"
             # print(a1)
+            # p = multi(a1,b)
         else:
             p = " "+b+" operation of the matricess is not possible"
     return p
+
+def add_sub(a1,b):
+    f1,m = [],0
+    for i in a1:
+        n = 0
+        d1 = []
+        for j in range(i[1]):
+            c1 = []
+            for k in range(i[0]):
+                n += 1
+                print("Enter the value of "+chr(65+m)+str(n))
+                inp = input()
+                inp1 = inp.isdigit()
+                while inp1:
+                    c1 +=[float(inp)]
+                    inp1 = False                
+            d1 += [c1]
+        f1 += [d1]
+        m += 1
+    return f1
+
+
+# def multi(a1,b):
 
 print("Enter matricess:")
 a = int(input())
